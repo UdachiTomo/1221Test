a01a6518a662ee40b61f372e8f6ae3dd76c30c9f
UdachiTomo <fragnet1ck@gmail.com>
2024-08-12 18:13:03 +0300

    1. UI верстка отображения элементов списком

---
+++ 1221Test/1221Test/Model/CardModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardModel.swift
+//  1221Test
+//
+//  Created by udachi_tomo on 12.08.2024.
+//
+
+import Foundation
---
+++ 1221Test/1221Test/ViewModel/CardViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardViewModel.swift
+//  1221Test
+//
+//  Created by udachi_tomo on 12.08.2024.
+//
+
+import Foundation
