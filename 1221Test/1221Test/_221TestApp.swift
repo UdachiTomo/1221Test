f99bd3863c1052c4d20f2351cd9d0238b1f207aa
UdachiTomo <fragnet1ck@gmail.com>
2024-08-12 14:27:37 +0300

    first commit

---
+++ 1221Test/1221Test/_221TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  _221TestApp.swift
+//  1221Test
+//
+//  Created by udachi_tomo on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct _221TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
