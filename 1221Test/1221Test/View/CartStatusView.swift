dee6d3c2bbe0214ea8364dfe7e696fa7fd5a43af
UdachiTomo <fragnet1ck@gmail.com>
2024-08-20 21:19:35 +0300

    6. финальный рефакторинг 6.1 исправлен баг с тем что не сохранялось состояние продукта при выборе количества

---
+++ 1221Test/1221Test/View/CartStatusView.swift
@@ -0,0 +1,18 @@
+//
+//  CartStatusView.swift
+//  1221Test
+//
+//  Created by udachi_tomo on 20.08.2024.
+//
+
+import SwiftUI
+
+struct CartStatusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartStatusView()
+}
